fa6471e4f6b755f45687a204f16954539b03ebfe
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-04-02 14:00:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert("Something went wrong");
+    }
+  );
+}
