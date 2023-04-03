e8f6822ec832f012680e5af0ad03c2582c59719f
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-04-03 17:32:08 +0200

    Add marker to the map

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
 
-      var map = L.map("map").setView([latitude, longitude], 13);
+      const map = L.map("map").setView([latitude, longitude], 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -17,6 +17,23 @@ if (navigator.geolocation) {
         .addTo(map)
         .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
         .openPopup();
+
+      map.on("click", (e) => {
+        const clicked = e.latlng;
+        L.marker(clicked)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 100,
+              minWidth: 50,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent(`<h2>${clicked.lat.toFixed(2)}</h2>`)
+          .openPopup();
+      });
     },
     function () {
       alert("Something went wrong");
