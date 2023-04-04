99cd2c2ef9b0b5be0542d99677d8fa169e3cd43e
Krzysztof Kiszka <krzysztofkiszka@yahoo.com>
2023-04-04 18:46:32 +0200

    Add workout form

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 "use strict";
 
+const form = document.querySelector(".form");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputEvelation = document.querySelector(".form__input--evelation");
+
+let map, clickedPoint;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
 
-      const map = L.map("map").setView([latitude, longitude], 13);
+      map = L.map("map").setView([latitude, longitude], 13);
 
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([latitude, longitude])
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      L.marker([latitude, longitude]).addTo(map);
 
-      map.on("click", (e) => {
-        const clicked = e.latlng;
-        L.marker(clicked)
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 100,
-              minWidth: 50,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent(`<h2>${clicked.lat.toFixed(2)}</h2>`)
-          .openPopup();
+      map.on("click", (eventMap) => {
+        clickedPoint = eventMap.latlng;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
     function () {
@@ -40,3 +35,28 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  inputDistance.value = inputDuration.value = inputCadence.value = "";
+  // Add makret to the map
+  L.marker(clickedPoint)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 100,
+        minWidth: 50,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent(`Workout: `)
+    .openPopup();
+});
+
+inputType.addEventListener("change", () => {
+  inputEvelation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
